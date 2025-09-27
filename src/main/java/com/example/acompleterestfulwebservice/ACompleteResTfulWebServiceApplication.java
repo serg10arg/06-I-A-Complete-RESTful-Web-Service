e912c1308dff63fbc5d35be6be0140d863696f1a
serg10arg <cabrerasergio094@gmail.com>
2025-09-27 10:34:13 -0300

    Initial commit

---
+++ src/main/java/com/example/acompleterestfulwebservice/ACompleteResTfulWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.acompleterestfulwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ACompleteResTfulWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ACompleteResTfulWebServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/acompleterestfulwebservice/ACompleteResTfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.acompleterestfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ACompleteResTfulWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
