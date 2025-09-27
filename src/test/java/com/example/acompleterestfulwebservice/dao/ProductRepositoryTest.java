e90ddbf6c964a310e721e6f7e998d45ed8a132f9
serg10arg <cabrerasergio094@gmail.com>
2025-09-27 17:22:15 -0300

    implementacion de pruebas unitarias para probar el efecto de Transactional

--- src/test/java/com/example/acompleterestfulwebservice/dao/ProductRepositoryTest.java
+++ src/test/java/com/example/acompleterestfulwebservice/dao/ProductRepositoryTest.java
@@ -1,4 +1,64 @@
+package com.example.acompleterestfulwebservice.dao;
+
+import com.example.acompleterestfulwebservice.entities.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@Transactional
+@SpringBootTest
 class ProductRepositoryTest {
-  
+
+    @Autowired
+    private ProductRepository repository;
+
+    @Test
+    void countProducts() {
+        assertEquals(3, repository.count());
+    }
+
+    @Test
+    void findById() {
+        assertTrue(repository.findById(1L).isPresent());
+    }
+
+    @Test
+    void findAll() {
+        List<Product> products = repository.findAll();
+        assertEquals(3, products.size());
+    }
+
+    @Test
+    void insertProduct() {
+        Product bat = new Product("cricket bat", BigDecimal.valueOf(35.00));
+        repository.save(bat);
+        assertAll(
+                () -> assertNotNull(bat.getId()),
+                () -> assertEquals(4, repository.count())
+        );
+    }
+
+    @Test
+    void deleteProduct() {
+        repository.deleteById(1L);
+        assertEquals(2, repository.count());
+    }
+
+    @Test
+    void deleteAllInBatch() {
+        repository.deleteAllInBatch();
+        assertEquals(0, repository.count());
+    }
+
+    @Test
+    void deleteAllProducts() {
+        repository.deleteAll();
+        assertEquals(0, repository.count());
+    }
 }
