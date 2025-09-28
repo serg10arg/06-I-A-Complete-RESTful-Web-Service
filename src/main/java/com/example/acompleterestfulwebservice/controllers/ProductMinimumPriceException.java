7ed1ab9cd9a8b14a70069ce2fec6d9f27983c11c
serg10arg <cabrerasergio094@gmail.com>
2025-09-27 22:36:01 -0300

    implementacion de Exception para manejar valores menores a 0 en el metodo getProductByMinPrice

--- src/main/java/com/example/acompleterestfulwebservice/controllers/ProductMinimumPriceException.java
+++ src/main/java/com/example/acompleterestfulwebservice/controllers/ProductMinimumPriceException.java
@@ -1,7 +1,16 @@
 package com.example.acompleterestfulwebservice.controllers;
 
 public class ProductMinimumPriceException extends RuntimeException {
-  public ProductMinimumPriceException(String message) {
-    super(message);
-  }
+    public ProductMinimumPriceException() {
+
+        this("Minimum price must be greater than zero");
+    }
+
+    public ProductMinimumPriceException(String message) {
+        super(message);
+    }
+
+    public ProductMinimumPriceException(double minPrice) {
+        this("Minimum price must be greater than zero, but was " + minPrice);
+    }
 }
